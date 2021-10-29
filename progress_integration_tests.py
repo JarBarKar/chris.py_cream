57e9c743c30c1e77a580c83dd03ec1a247145c69
Alvan Tan <alvan.tan.2019@sis.smu.edu.sg>
2021-10-29 17:42:58 +0800

    Update progress_integration_tests.py

--- progress_integration_tests.py
+++ progress_integration_tests.py
@@ -424,7 +424,7 @@ class TestViewLessonContentStatus(TestApp):
 
 if __name__ == '__main__':
     #For jenkins
-    # import xmlrunner
-    # unittest.main(testRunner=xmlrunner.XMLTestRunner(output='test-reports'))
+    import xmlrunner
+    unittest.main(testRunner=xmlrunner.XMLTestRunner(output='test-reports'))
     #For local tests
-    unittest.main()
+    # unittest.main()
