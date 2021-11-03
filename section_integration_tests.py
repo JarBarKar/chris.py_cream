ed29be8ec93e484ec05c1a43c27a46cc2576e8f1
Jian Cheng <shi.jiancheng0@gmail.com>
2021-11-03 15:37:36 +0800

    update section test
    retry to fix merge issue

--- section_integration_tests.py
+++ section_integration_tests.py
@@ -780,5 +780,6 @@ if __name__ == '__main__':
     #For jenkins
     import xmlrunner
     unittest.main(testRunner=xmlrunner.XMLTestRunner(output='test-reports'))
+    
     #For local tests
     # unittest.main()
